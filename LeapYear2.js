75010aad30b9543558b1e1e6552a6047452f24fa
ImOBxx <166965179+ImOBxx@users.noreply.github.com>
2024-09-15 08:50:24 +0530

    Add files via upload

---
+++ LeapYear2.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log()
+rl.question('Enter a year: ', (input) => {
+  const year = parseInt(input);
+  console.log()
+
+  if (isNaN(year)) {
+    console.log('Please enter a valid number.');
+  } else {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      console.log(`${year} is a leap year.`);
+    } else {
+      console.log(`${year} is not a leap year.`);
+    }
+  }
+
+  rl.close();
+  console.log()
+});
---
+++ Test.js
@@ -0,0 +1,4 @@
+var x = 45
+var y = 45
+sum = x * y
+console.log(sum)
